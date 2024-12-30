0c897587cdf313302363e2ee6cffd6c79c1882a5
lsdvitamin <lsdvitamin7@gmail.com>
2024-12-30 20:50:59 +0300

    Реализовал hw2

--- src/test/java/ru/otus/bank/service/impl/PaymentProcessorImplTest.java
+++ src/test/java/ru/otus/bank/service/impl/PaymentProcessorImplTest.java
@@ -2,6 +2,7 @@ package ru.otus.bank.service.impl;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.ArgumentMatcher;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -9,12 +10,15 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import ru.otus.bank.entity.Account;
 import ru.otus.bank.entity.Agreement;
 import ru.otus.bank.service.AccountService;
+import ru.otus.bank.service.exception.AccountException;
 
 import java.math.BigDecimal;
 import java.util.List;
 
-import static org.mockito.Mockito.argThat;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class PaymentProcessorImplTest {
@@ -59,5 +63,89 @@ public class PaymentProcessorImplTest {
                 0, 0, BigDecimal.ONE);
 
     }
+
+
+
+@Test
+public void makeTransferSourceExceptionTest() {
+    AccountException exception = new AccountException("Account not found");
+    Agreement sourceAgreement = new Agreement();
+    sourceAgreement.setId(1L);
+    Agreement destinationAgreement = new Agreement();
+    destinationAgreement.setId(2L);
+    when(accountService.getAccounts(any())).thenThrow(exception);
+    AccountException result = assertThrows(AccountException.class, () ->
+            paymentProcessor.makeTransfer(sourceAgreement, destinationAgreement, 0, 0, new BigDecimal(100)));
+
+    assertEquals(exception.getLocalizedMessage(), result.getLocalizedMessage());
+}
+
+    @Test
+    public void makeTransferDestinationExceptionTest() {
+        AccountException exception = new AccountException("Destination account not found");
+        Agreement sourceAgreement = new Agreement();
+        sourceAgreement.setId(1L);
+
+        Agreement destinationAgreement = new Agreement();
+        destinationAgreement.setId(2L);
+
+        Account sourceAccount = new Account();
+        sourceAccount.setAmount(new BigDecimal(10));
+        sourceAccount.setType(1);
+
+        lenient().when(accountService.getAccounts(argThat(argument ->
+                argument != null && argument.getId() == 1L))).thenReturn(List.of(sourceAccount));
+
+        when(accountService.getAccounts(any(Agreement.class))).thenThrow(exception);
+        AccountException result = assertThrows(AccountException.class, () ->
+                paymentProcessor.makeTransfer(sourceAgreement, destinationAgreement, 0, 0, new BigDecimal(100)));
+        assertEquals(exception.getLocalizedMessage(), result.getLocalizedMessage());
+    }
+
+    @Test
+    public void makeTransferWithComissionSourceExceptionTest() {
+        AccountException exception = new AccountException("Account not found");
+        Agreement sourceAgreement = new Agreement();
+        sourceAgreement.setId(1L);
+        Agreement destinationAgreement = new Agreement();
+        destinationAgreement.setId(2L);
+        when(accountService.getAccounts(any())).thenThrow(exception);
+        AccountException result = assertThrows(AccountException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                paymentProcessor.makeTransferWithComission(sourceAgreement, destinationAgreement, 0,
+                        0, new BigDecimal(10), new BigDecimal(1));
+            }
+        });
+
+        assertEquals(exception.getLocalizedMessage(), result.getLocalizedMessage());
+    }
+
+    @Test
+    public void makeTransferDestinationWithComissionSourceExceptionTest() {
+        AccountException exception = new AccountException("Destination account not found");
+        Agreement sourceAgreement = new Agreement();
+        sourceAgreement.setId(1L);
+
+        Agreement destinationAgreement = new Agreement();
+        destinationAgreement.setId(2L);
+
+        Account sourceAccount = new Account();
+        sourceAccount.setAmount(new BigDecimal(100));
+        sourceAccount.setType(0);
+
+        lenient().when(accountService.getAccounts(argThat(new ArgumentMatcher<Agreement>() {
+            @Override
+            public boolean matches(Agreement argument) {
+                return argument != null && argument.getId() == 1L;
+            }
+        }))).thenReturn(List.of(sourceAccount));
+        when(accountService.getAccounts(any(Agreement.class))).thenThrow(exception);
+        AccountException result = assertThrows(AccountException.class, () ->
+                paymentProcessor.makeTransferWithComission(sourceAgreement, destinationAgreement, 0,
+                        0, new BigDecimal(100), new BigDecimal(100)));
+        assertEquals(exception.getLocalizedMessage(), result.getLocalizedMessage());
+    }
+
 
 }
